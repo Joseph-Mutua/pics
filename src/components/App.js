9b7a4c644073e9ceb6ad08d0d35231e0b0768370
Joseph Mutua <Mutuaj793@gmail.com>
2020-11-16 18:14:46 +0300

    Fetching data with axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID DYzdl-JNf8bfv0ZFpHkeHDiYBMUJS33M9Qq1EZW5tsU",
+      },
+    });
   }
 
   state = {};
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar onSubmit={this.onSearchSubmit}/>
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
