030b994f90d45efe0aec87d09a03cd680af39f7c
Joseph Mutua <Mutuaj793@gmail.com>
2020-11-16 18:24:40 +0300

    Handling requests with async await

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID DYzdl-JNf8bfv0ZFpHkeHDiYBMUJS33M9Qq1EZW5tsU",
       },
     });
+    console.log(response.data.results);
   }
 
   state = {};
